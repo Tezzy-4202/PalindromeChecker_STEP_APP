c5bed252d998a0a3d67a9f0a7a5983a1853c63b3
Tezzy_4204 <arun.t3ja@gmail.com>
2026-02-19 10:16:52 +0530

    02 UC1 - Application Entry & Welcome Message

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome Checker App");
+        System.out.println("Version: 1.0");
+        System.out.println("Status: System Initialized.");
+    }
+}

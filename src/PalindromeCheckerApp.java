adda25774dfaf2681961c80e63be37ce955f5e56
Arun Teja <97935516+Tezzy-4202@users.noreply.github.com>
2026-02-26 09:03:19 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
-    public static void main(String[] args) {
+    public static void PalindromeCheckerApp(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a long string for performance test: ");
         String input = scanner.nextLine();

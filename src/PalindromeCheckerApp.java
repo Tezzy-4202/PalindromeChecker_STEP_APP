0aa657243152bd3369b1c592be76a08f09d9a8fd
Arun Teja <97935516+Tezzy-4202@users.noreply.github.com>
2026-02-26 09:02:38 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -28,7 +28,7 @@ class TwoPointerStrategy implements PalindromeStrategy {
 }
 
 public class PalindromeCheckerApp {
-    public static void main(String[] args) {
+    public static void PalindromeCheckerApp(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter string: ");
         String input = scanner.nextLine();
